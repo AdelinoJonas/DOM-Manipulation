35a8fc3403a310e2dfc6006a737619de5fea490a
AdelinoJonas <84252481+AdelinoJonas@users.noreply.github.com>
2021-10-18 22:43:04 -0300

    Add files via upload

---
+++ eventos/filtro_de_produtos/script.js
@@ -0,0 +1,31 @@
+const frutas = document.querySelectorAll('li');
+
+function buscarFruta(event) {
+
+    if (!teclouEnter(event.code)) {
+        return;
+    }
+    filtrarLista(frutas, event.target.value);
+
+    limparInput(event.target);
+}
+
+function teclouEnter(tecla) {
+    return tecla === 'Enter';
+}
+
+function filtrarLista(lista, filtro) {
+    frutas.forEach(item => {
+
+        item.classList.remove('escondido');
+
+        if (filtro && item.textContent != filtro) {
+
+            item.classList.add('escondido');
+        }
+    });
+}
+
+function limparInput() {
+    input.value = '';
+}
